80909ba9ec41bf895032b45d365a1e8427f425d2
Prakash Subramanian <spakai@gmail.com>
2025-06-23 13:47:07 +0800

    Update user_data.sh

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -8,7 +8,7 @@ sudo service docker start
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/bin/docker-compose && sudo chmod +x /usr/bin/docker-compose && docker-compose --version
 
 export GITHUB_PAT=$(aws secretsmanager get-secret-value --secret-id github_pat20 --region "us-east-1" | jq -r .SecretString)
-sudo git clone -b codex/write-terraform-code-for-aws-setup https://spakai:$GITHUB_PAT@github.com/spakai/service_mesh_demo.git app
+sudo git clone  https://spakai:$GITHUB_PAT@github.com/spakai/service_mesh_demo.git app
 cd app
 sudo docker-compose up -d --build
 
